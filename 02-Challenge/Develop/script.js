eb2a8aa716282e052ae41eebbab3e03150fbbc6d
AdamLebo <alieberman10@aol.com>
2022-10-18 22:33:41 -0400

    updated js

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,3 +1,9 @@
 var today = moment().format('MMMM Do YYYY');
 
 $('#currentDay').text(today);
+
+$(".saveBtn").on("click", function(){
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+})
